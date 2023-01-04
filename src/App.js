71b18a69cfc4ebbc1e69ab31d0db75d43d0e723d
Mamitiana <mamitiana@fulldigits.com>
2023-01-04 08:57:55 +0300

    Pre 2:03:02 _ rsl OK _ Showed the Address of the NFTs

--- src/App.js
+++ src/App.js
@@ -77,10 +77,11 @@ function App() {
               <div className="card__info">
                 <h4>{home.attributes[0].value} ETH</h4>
                 <p>
-                  <strong>1</strong> bds |<strong>2</strong> ba |
-                  <strong>3</strong> sqft
+                  <strong>{home.attributes[2].value}</strong> bds |
+                  <strong>{home.attributes[3].value}</strong> ba |
+                  <strong>{home.attributes[4].value}</strong> sqft
                 </p>
-                <p>1234 St</p>
+                <p>{home.address}</p>
               </div>
             </div>
           ))}
