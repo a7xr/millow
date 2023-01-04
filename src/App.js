4c75d835f5f57a56417100b895598e39688ba4d1
Mamitiana <mamitiana@fulldigits.com>
2023-01-04 08:24:15 +0300

    Pre 1:51:39 _ rsl OK _ Show box for the NFT

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Escrow from "./abis/Escrow.json";
 import config from "./config.json";
 
 function App() {
+  const [home, setHome] = useState({});
   const [account, setAccount] = useState(null);
 
   const loadBlockchainData = async () => {
@@ -37,7 +38,24 @@ function App() {
       <Navigation account={account} setAccount={setAccount} />
       <Search />
       <div className="cards__section">
-        <h3>Welcome to Millow !!</h3>
+        <h3>Homes for you</h3>
+        <hr />
+        <div className="cards">
+          <div className="card">
+            <div className="card__image">
+              <img src={home.image} alt="Home" />
+            </div>
+            <div className="card__info">
+              <h4>1 ETH</h4>
+              <p>
+                <strong>1</strong> bds |
+                <strong>2</strong> ba |
+                <strong>3</strong> sqft
+              </p>
+              <p>1234 St</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
