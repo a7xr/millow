abffd9d6a8448de2fd5cb77d863132d6446a72a2
Mamitiana <mamitiana@fulldigits.com>
2023-01-04 07:51:51 +0300

    Pre 1:43:43 _ rsl OK

--- src/App.js
+++ src/App.js
@@ -31,8 +31,9 @@ function App() {
 
   return (
     <div>
+      <Navigation />
       <div className="cards__section">
-        <h3>Welcome to Millow !</h3>
+        <h3>Welcome to Millow !!</h3>
       </div>
     </div>
   );
